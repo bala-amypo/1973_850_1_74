717d68ee74fe416fce15903ea618ebe749ee60eb
bala-amypo <bala@amypo.in>
2025-12-20 14:40:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/FraudCheckResultRepository.java
+++ src/main/java/com/example/demo/repository/FraudCheckResultRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
 
-public interface FraudCheckResultRepository{
-    
+import com.example.demo.model.FraudCheckResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface FraudCheckResultRepository extends JpaRepository<FraudCheckResult, Long> {
+    Optional<FraudCheckResult> findByClaimId(Long claimId); // [cite: 194]
 }
